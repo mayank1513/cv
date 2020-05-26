4f52ee139444b8d44543898326aa71a349f2d9f0
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2020-05-26 18:34:03 +0530

    first-commit

---
+++ precache-manifest.6781e634cf99ac51d01ae3c4ee525d0b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f03e11de22ee1cd7da8",
+    "url": "/css/app.68bf3639.css"
+  },
+  {
+    "revision": "4a86343bd8294bfb9527",
+    "url": "/css/chunk-vendors.1392eb48.css"
+  },
+  {
+    "revision": "444762b414c3279260366de57c23b970",
+    "url": "/img/cv.444762b4.gif"
+  },
+  {
+    "revision": "6a625ec0b13bfd110482c56ae74312d9",
+    "url": "/img/gearstar_ani.6a625ec0.gif"
+  },
+  {
+    "revision": "ddef13679f04075b1fb0b7b1209c7966",
+    "url": "/img/logo.ddef1367.png"
+  },
+  {
+    "revision": "aac36db5c71173a98a5a42fabe2a6370",
+    "url": "/img/trophy_1024.aac36db5.gif"
+  },
+  {
+    "revision": "a7200d43dfc5e4907372c08511575090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f03e11de22ee1cd7da8",
+    "url": "/js/app.89bd5312.js"
+  },
+  {
+    "revision": "4a86343bd8294bfb9527",
+    "url": "/js/chunk-vendors.9da93082.js"
+  },
+  {
+    "revision": "4fd5c4eb2ba73f86614bbbf9b6954a6f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
